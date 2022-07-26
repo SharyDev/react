3f9346fc2e86c0911bf489d95d3fd5e438a33e4a
SharyDev <shahroz.abbas2018@gmail.com>
2022-07-26 17:28:07 +0500

    json-server and fetch data from server

--- Components/DishDetailComponent.js
+++ Components/DishDetailComponent.js
@@ -7,6 +7,7 @@ import {
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../Shared/baseUrl';
 
 
 
@@ -14,7 +15,7 @@ import { Loading } from './LoadingComponent';
 function RenderDish({ dish }) {
     return (
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+             <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
--- Components/MenuComponents.js
+++ Components/MenuComponents.js
@@ -3,12 +3,13 @@ import { Card, CardImg, CardImgOverlay,
     CardTitle,Breadcrumb, BreadcrumbItem  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../Shared/baseUrl';
 
 function RenderMenuItem ({dish, onClick}) {
     return (
         <Card>
             <Link to={`/menu/${dish.id}`} >
-                <CardImg width="20%" src={dish.image} alt={dish.name} />
+                <CardImg width="20%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
